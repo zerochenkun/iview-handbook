ed6ae0aea3a7d4703f187e518191093ffdd88d41
zerochenkun <chenuvi@outlook.com>
2021-01-27 19:06:18 +0800

    Feat: 使用VueRouter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <ul>
+        <li>
+          <router-link to="/like">实战 1：具有数据校验功能的表单组件 —— Form</router-link>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/views/form.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>form.vue</div>
+</template>
+
+<script>
+export default {
+  name: 'form'
+}
+</script>
+
+<style>
+</style>
